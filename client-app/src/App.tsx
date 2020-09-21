232760a5876d355cbc6a1daab8650966cffa3ef4
Keith Pounds <kpounds@greenmountaintechnology.com>
2020-09-21 16:44:08 -0500

    Adds semantic UI components

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from "react"
-import logo from "./logo.svg"
 import "./App.css"
 import { Value } from "./models/Value"
 import axios from "axios"
+import { Header, Icon, Item, List } from "semantic-ui-react"
 
 const App: React.FunctionComponent = () => {
   const [values, setValues] = useState<Value[]>([])
 
   useEffect(() => {
     axios.get("http://localhost:5000/api/values").then((response) => {
-      console.log("response", response)
       setValues(response.data)
     })
   }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {values.map((value) => (
-            <li key={value.id}>{value.name}</li>
-          ))}
-        </ul>
-      </header>
+    <div>
+      <Header as="h2">
+        <Icon name="users" />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {values.map((value) => (
+          <Item key={value.id}>{value.name}</Item>
+        ))}
+      </List>
     </div>
   )
 }
