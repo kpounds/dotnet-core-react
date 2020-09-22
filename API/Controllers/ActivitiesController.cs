3dead3101a2a7e457688c204b9cce11f800d764f
Keith Pounds <kpounds@greenmountaintechnology.com>
2020-09-22 13:46:39 -0500

    adds edit command

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
+using Application.Activities.Commands;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -33,6 +34,13 @@ namespace API.Controllers
     [HttpPost]
     public async Task<ActionResult<Unit>> Create(Create.Command command)
     {
+      return await _mediator.Send(command);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+    {
+      command.Id = id;
       return await _mediator.Send(command);
     }
   }
