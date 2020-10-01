bc1f02b561d49ad814fd836cf4dc6ca41f2fa5ce
Keith Pounds <kpounds@greenmountaintechnology.com>
2020-10-01 16:46:43 -0500

    moving home page outside of navigation routes

--- client-app/src/layout/App.tsx
+++ client-app/src/layout/App.tsx
@@ -23,13 +23,20 @@ const App: FunctionComponent<RouteComponentProps> = ({ location }) => {
 
   return (
     <>
-      <NavBar />
-      <Container className="app-container">
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/activities" component={ActivityDashboard} />
-        <Route path="/activities/:id" component={ActivityDetails} />
-        <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityForm} />
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <NavBar />
+            <Container className="app-container">
+              <Route exact path="/activities" component={ActivityDashboard} />
+              <Route path="/activities/:id" component={ActivityDetails} />
+              <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityForm} />
+            </Container>
+          </>
+        )}
+      />
     </>
   )
 }
